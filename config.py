cb737013b1f3da683c90a09f34816565fbf063e2
ARUNCO <duncanarani@gmail.com>
2018-09-18 11:01:20 +0300

    deploying

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ class Config:
     General configuration parent class
     '''
     SECRET_KEY = 'os.environ.get'
-    # SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://dunco:dunco@localhost/blog'
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://dunco:dunco@localhost/blog'
     UPLOADED_PHOTOS_DEST ='app/static/photos'
     
     #  email configurations
